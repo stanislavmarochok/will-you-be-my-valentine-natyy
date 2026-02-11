02334d89b7a8aa518cd31e200f71ebe4d2d35341
Stanislav Marochok <ssssstas30@gmail.com>
2026-02-11 14:23:07 +0100

    updated base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/valentines-story/' // <-- replace with your repo name
+  base: '/will-you-be-my-valentine-natyy/'
 })
